5c13fbca73b3a3ac32d1c15119d076f74157676c
klose911 <klose911@gmail.com>
2025-07-20 00:08:21 +0800

    fix the bug of wrong function declaration of rbswrk

--- user/usertests.c
+++ user/usertests.c
@@ -244,8 +244,9 @@ copyinstr3(char *s)
 // See if the kernel refuses to read/write user memory that the
 // application doesn't have anymore, because it returned it.
 void
-rwsbrk()
+rwsbrk(char* args)
 {
+  (void) args; // bypass compile warning for unused variable 
   int fd, n;
   
   uint64 a = (uint64) sbrk(8192);
